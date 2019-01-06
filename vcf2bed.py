78a3184e7eb16535662e11c1ea8e733917c78b3d
hwangjc <hwangjc@umich.edu>
2019-01-05 19:24:08 -0500

    changed default output to stdout

--- vcf2bed.py
+++ vcf2bed.py
@@ -4,7 +4,7 @@ import argparse
 ##### parse arguments
 parser = argparse.ArgumentParser(description="Converters a .vcf file to a .bed file.");
 parser.add_argument('-i', '--input', required=True, help="The name of the input .vcf file.");
-parser.add_argument('-o', '--output', required=False, help="The name of the file to output to (will overwrite file if it already exists). If this argument is not specified, output will be to the input file name with a .bed extension");
+parser.add_argument('-o', '--output', required=False, help="The name of the file to output to (will overwrite file if it already exists). If this argument is not specified, output will be to stdout");
 
 _args = parser.parse_args();
 globals()['args'] = _args;
@@ -23,16 +23,16 @@ if args.input == args.output:
 vcf_file = open(args.input);
 if args.output != None:
     bed_file_name = args.output;
-else:
-    bed_file_name = args.input[:-4] + ".bed";
-bed_file = open(bed_file_name, "w");
+    bed_file = open(bed_file_name, "w");
+
 
 for line in vcf_file:
     firstTabIndex = line.index('\t');
     secondTabIndex = line.index('\t', firstTabIndex + 1);
     pos = int(line[firstTabIndex:secondTabIndex]);
-    bed_file.write('\t'.join(["chr" + line[0:firstTabIndex], str(pos - 1), str(pos), line[secondTabIndex + 1:]]));
-
-print("Outputed to " + bed_file_name);
+    if args.output != None:
+        bed_file.write('\t'.join(["chr" + line[0:firstTabIndex], str(pos - 1), str(pos), line[secondTabIndex + 1:]]));
+    else:
+        print('\t'.join(["chr" + line[0:firstTabIndex], str(pos - 1), str(pos), line[secondTabIndex + 1:]]));
 
 #########################
